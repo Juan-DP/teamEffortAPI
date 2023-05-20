74f43f0b17c12e41feaeb6de73c229da827ab5d6
Juan Pestana <juanpestana21@gmail.com>
2023-05-20 18:00:31 +0200

    chore(boiler plate controllers)

--- app/Http/Controllers/DonationController.php
+++ app/Http/Controllers/DonationController.php
@@ -6,5 +6,83 @@ use Illuminate\Http\Request;
 
 class DonationController extends Controller
 {
-    //
+    /**
+     * Get all the challenges
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws conditon
+     **/
+    public function index(Request $request)
+    {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Get a challenge by its uid
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws conditon
+     **/
+    public function show(Request $request)
+    {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Create a challenge
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws conditon
+     **/
+    public function create(Request $request)
+    {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Update a challenge.
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws conditon
+     **/
+    public function update(Request $request)
+    {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
+    /**
+     * Delete a challenge.
+     *
+     * @param Request $request
+     * @return mixed
+     * @throws conditon
+     **/
+    public function delete(Request $request)
+    {
+        try {
+            //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChallengeController;
+use App\Http\Controllers\CharityController;
+use App\Http\Controllers\DonationController;
+use App\Http\Controllers\TeamController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +21,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('challenge', ChallengeController::class);
+Route::apiResource('charity', CharityController::class);
+Route::apiResource('donation', DonationController::class);
+Route::apiResource('team', TeamController::class);
